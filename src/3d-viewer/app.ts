942054fc4f0d7e0703cc953b95068c0c27a2fa9a
Anton Petrov <optsing@outlook.com>
2020-07-04 01:13:40 +0300

    Small refactor

--- src/3d-viewer/app.ts
+++ src/3d-viewer/app.ts
@@ -2,6 +2,10 @@ import './app.css';
 
 import qs from 'qs';
 
+import { Scene, Color, PerspectiveCamera, WebGLRenderer, AmbientLight, PointLight, Object3D, Box3, Vector3 } from 'three';
+import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 function findFileName (url: string) {
     const lastSlash = url.lastIndexOf('/');
     if (lastSlash > -1) {
@@ -10,65 +14,12 @@ function findFileName (url: string) {
     return url;
 }
 
-const { src } = qs.parse(location.search, { ignoreQueryPrefix: true }) as { [key: string]: string };
-
-document.title = findFileName(src);
-
-import { Scene, Color, PerspectiveCamera, WebGLRenderer, AmbientLight, PointLight, Object3D, Box3, Vector3 } from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-const scene = new Scene();
-scene.background = new Color(0xeeeeee);
-
-const camera = new PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
-
-const renderer = new WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
-
-const hlight = new AmbientLight(0x404040, 1);
-scene.add(hlight);
-
-// const directionalLight = new THREE.DirectionalLight(0xffffff, 100);
-// directionalLight.position.set(0, 1, 0);
-// directionalLight.castShadow = true;
-// scene.add(directionalLight);
-
-const light = new PointLight(0xc4c4c4, 1);
-light.position.set(0, 300, 500);
-scene.add(light);
-
-const light2 = new PointLight(0xc4c4c4, 1);
-light2.position.set(500, 100, 0);
-scene.add(light2);
-
-const light3 = new PointLight(0xc4c4c4, 1);
-light3.position.set(0, 100, -500);
-scene.add(light3);
-
-const light4 = new PointLight(0xc4c4c4, 1);
-light4.position.set(-500, 300, 500);
-scene.add(light4);
-
-const light5 = new PointLight(0xc4c4c4, 1);
-light5.position.set(0, 0, 0);
-scene.add(light5);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-function fitCameraToObject (camera: PerspectiveCamera, object: Object3D, offset: number, controls: OrbitControls) {
-
-    offset = offset || 1.25;
-
-    const boundingBox = new Box3();
-
+function fitCameraToObject (camera: PerspectiveCamera, object: Object3D, offset: number = 1.25, controls?: OrbitControls) {
     // get bounding box of object - this will be used to setup controls and camera
+    const boundingBox = new Box3();
     boundingBox.setFromObject(object);
 
     const center = boundingBox.getCenter(new Vector3());
-
     const size = boundingBox.getSize(new Vector3());
 
     // get the max side of the bounding box (fits to width OR height as needed )
@@ -87,40 +38,90 @@ function fitCameraToObject (camera: PerspectiveCamera, object: Object3D, offset:
     camera.updateProjectionMatrix();
 
     if (controls) {
-
         // set camera to rotate around center of loaded object
         controls.target = center;
-
         // prevent camera from zooming out far enough to create far plane cutoff
         controls.maxDistance = cameraToFarEdge * 2;
-
         controls.saveState();
-
     } else {
-
         camera.lookAt(center);
-
     }
 }
 
-function animate () {
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
+async function loadGTFL (loader: GLTFLoader, src: string): Promise<GLTF> {
+    return await new Promise<GLTF>(resolve => {
+        loader.load(src, gltf => {
+            resolve(gltf);
+        });
+    });
 }
 
-const loader = new GLTFLoader();
-loader.load(src, function (gltf) {
+async function main () {
+    const { src } = qs.parse(location.search, { ignoreQueryPrefix: true }) as { [key: string]: string };
+
+    document.title = findFileName(src);
+
+    const scene = new Scene();
+    scene.background = new Color(0xeeeeee);
+
+    const camera = new PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+
+    const renderer = new WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+    const hlight = new AmbientLight(0x404040, 1);
+    scene.add(hlight);
+
+    // const directionalLight = new THREE.DirectionalLight(0xffffff, 100);
+    // directionalLight.position.set(0, 1, 0);
+    // directionalLight.castShadow = true;
+    // scene.add(directionalLight);
+
+    const light = new PointLight(0xc4c4c4, 1);
+    light.position.set(0, 300, 500);
+    scene.add(light);
+
+    const light2 = new PointLight(0xc4c4c4, 1);
+    light2.position.set(500, 100, 0);
+    scene.add(light2);
+
+    const light3 = new PointLight(0xc4c4c4, 1);
+    light3.position.set(0, 100, -500);
+    scene.add(light3);
+
+    const light4 = new PointLight(0xc4c4c4, 1);
+    light4.position.set(-500, 300, 500);
+    scene.add(light4);
+
+    const light5 = new PointLight(0xc4c4c4, 1);
+    light5.position.set(0, 0, 0);
+    scene.add(light5);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    function animate () {
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+    }
+
+    window.addEventListener('resize', () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(width, height);
+    }, { passive: true });
+
+    const loader = new GLTFLoader();
+    const gltf = await loadGTFL(loader, src);
+
     const model = gltf.scene.children[0];
     scene.add(gltf.scene);
     fitCameraToObject(camera, model, 1, controls);
     animate();
-});
+}
 
-window.addEventListener('resize', () => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(width, height);
-}, { passive: true });
+main();
